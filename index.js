18ea3964851832a8512d4c6db170832084adb13d
Danbradleystakusopere <danstakus@gmail.com>
2025-06-08 22:21:08 +0300

    updated

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const divide = function() {
+  return 2000 / 100;
+};
+
+const square = num => num * num;
+
+const add = (a, b) => a + b;
+
+
+module.exports = {
+  divide,
+  square,
+  add
+};
